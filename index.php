621f1dedde3fce2202ee9ca5b6383ae7cc3150d9
SyafiaAzreen <syafiaazreen323@gmail.com>
2021-10-04 12:52:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,19 +10,106 @@
     <h1>Asas PHP</h1>
     
     <?php 
-    //komen satu baris
-    #komen satu baris
+    // komen satu baris
+    # komen satu baris
     /* 
-        komen banyak baris
+    komen banyak baris
     */
 
-    $namapelajar = 'Syafia';
-    echo '<h2>Hello $namapelajar </h2>'; #output
+    $namapelajar ='Siti Noryafia Azreen';
+    echo "<h2>Hello $namapelajar</h2>"; #output
 
     $hadir = false;
     $umur = 20;
     $nokp = '010904070546';
-
     ?>
+
+<?php 
+    //addition
+    $x = 10;  
+    $y = 6;
+
+    echo $x + $y;
+    echo "<br>";
+    
+
+    //subtraction
+    $x = 10;  
+    $y = 6;
+
+    echo $x - $y;
+    echo "<br>";
+
+    //multiplication
+    $x = 10;  
+    $y = 6;
+
+    echo $x * $y;
+    echo "<br>";
+
+    //Division
+    $x = 10;  
+    $y = 6;
+
+    echo $x / $y;
+    echo "<br>";
+
+    //Modulus
+    $x = 10;  
+    $y = 6;
+
+    echo $x % $y;
+    echo "<br>";
+    ?>
+<p>
+<?php
+$x = 5;  
+$y = 10;
+
+echo ($x <=> $y); // returns -1 because $x is less than $y
+echo "<br>";
+
+$x = 10;  
+$y = 10;
+
+echo ($x <=> $y); // returns 0 because values are equal
+echo "<br>";
+
+$x = 15;  
+$y = 10;
+
+echo ($x <=> $y); // returns +1 because $x is greater than $y
+?>  
+</p>
+
+<?php
+$ibubapa ='ibu'; # ibu /bapa/ kedua - duanya
+$yatim = 500;
+$piatu = 1000;
+
+if ($ibubapa == 'ibu' xor $ibubapa = 'bapa') {
+    echo 'Anak yatim , dapat RM' ,$yatim;
+} else {
+    #
+}
+?>
+
+<p>
+    <?php
+    echo 20 . 21;
+    ?>
+
+</p>
+
+<h2>
+    <?php
+    $bandar = 'Arau';
+    $baru = 'Bandar ';
+    for($x =0; $x < strlen($bandar); $x++) {
+        $baru .=$bandar[$x] . ' ';
+    }
+    echo $baru;
+    ?>
+</h2>
 </body>
 </html>
